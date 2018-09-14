568e11d630ab7166b6941ea70ad5e1262e495840
Gonzalo Roland <38432254+GonzaRoland@users.noreply.github.com>
2018-09-13 21:42:08 -0300

    Segundo push de Gonza Sprint 2

--- main.php
+++ main.php
@@ -1,10 +1,18 @@
 
-                <section class="banner">
-                    <img src="img/bannerspring.jpg">
-                </section>
-            </header>
-            <main>
-                    <div class="box-color_logo">NUESTROS PRODUCTOS</div>
+            
+            <div class="main-banner">
+                <img src="img/bannerspring_main_c.png">
+            </div>
+            <div class="social-icons">
+                <div class="icons">
+                <a href=""><img src="img/social-fb.png"></a>
+                <a href=""><img src="img/social-tw.png"></a>
+                <a href=""><img src="img/social-ig.png"></a>
+                <a href=""><img src="img/social-email.png"></a>
+                </div>
+            </div>  
+                <main>
+                    
                 
                 
 
@@ -12,33 +20,39 @@
                     
                     <article class="doggo">
                         <img class="doggo-pic" src="img/buzo_dogo.jpg">
-                        <p><strong>Indumentaria: Abrigos, Correas, collares y mas para tus Mascotas</strong><br></p>
-                        <a href="#">VER MÁS...</a>
+                        <h2>INDUMENTARIA</h2> 
+                        <p>Abrigos, Correas, collares y más para mantener a tu mascota segura, abrigada y a la moda<br></p>
+                        <a href="?pagina=catalogo">VER MÁS...</a>
                     </article>
                     <article class="doggo">
                         <img class="doggo-pic" src="img/almohadon_dogo.jpg">
-                        <p><strong>DecoPet: Acolchados, almohadones, camas, cuchas, canastos, etc.</strong><br></p>
-                        <a href="#">VER MÁS...</a>
+                        <h2>DECO-PET</h2>
+                        <p>Almohadones, camas, cuchas, canastos y todo lo necesario para mantener a tu mejor amigo cómodo y con estilo<br></p>
+                        <a href="?pagina=catalogo">VER MÁS...</a>
                     </article>
                     <article class="doggo">
                             <img class="doggo-pic" src="img/soga_dogo.jpg">
-                            <p><strong>Toys: Entretenimientos, Juguetes para Perros y Gatos.</strong><br></p>
-                            <a href="#">VER MÁS...</a>
+                            <h2>JUGUETES</h2>
+                            <p>Entretenimientos para que puedan compartir juntos los mejores momentos<br></p>
+                            <a href="?pagina=catalogo">VER MÁS...</a>
                     </article>
                     <article class="doggo">
                         <img class="doggo-pic" src="img/correa_app.jpg">
-                        <p><strong>Lo Ultimo en Tech, Correas Digitales, Dispensers...</strong><br></p>
-                        <a href="#">VER MÁS...</a>
+                        <h2>TECH</h2>
+                        <p>Correas automáticas, dispensers y todo lo último en tecnología para un cuidado moderno<br></p>
+                        <a href="?pagina=catalogo">VER MÁS...</a>
                     </article>
                     <article class="doggo">
                             <img class="doggo-pic" src="img/alimento_dogo.jpg">
-                            <p><strong>Alimentos: Balanceados para todo tipo de animales domesticos. Golosinas.</strong><br></p>
-                            <a href="#">VER MÁS...</a>
+                            <h2>ALIMENTOS</h2>
+                            <p>Balanceados de todo tipo y golosinas para que crezca alegre y bien nutrido<br></p>
+                            <a href="?pagina=catalogo">VER MÁS...</a>
                     </article>
                     <article class="doggo">
                             <img class="doggo-pic" src="img/antipulgas_dogo.jpg">
-                            <p><strong>Prodructos para la higiene, salud y el cuidado.</strong><br></p>
-                            <a href="#">VER MÁS...</a>
+                            <h2>SALUD E HIGIENE</h2>
+                            <p>Todo lo necesario para que tu mascota este siempre limpia y saludable<br></p>
+                            <a href="?pagina=catalogo">VER MÁS...</a>
                     </article>
                                         
                 </section>
@@ -55,21 +69,20 @@
 
                 <div class="overlay">
                     <p>
-                        CREÁ TU CUENTA CON NOSOTROS Y ACCEDÉ A GRANDES BENEFICIOS.
+                        CREÁ TU CUENTA Y ACCEDÉ A GRANDES BENEFICIOS.
                         <br>
                         <br>
-                        <a href="">REGISTRARME</a>
+                        <a href="dogo.php?pagina=registro">REGISTRARME</a>
                     </p>
                </div>
             </section>
         <footer>
             <nav class="footer-nav">
-                <a href="index.html">Home</a>
-                    <a href="#">Productos</a>
-                    <a href="frecuentes.html">FAQ</a>
-                    <a href="#">Contacto</a>
-                    <a href="login.html">Login</a>
-                    <a href="form.html">Regístrese</a>
+                <a href="?pagina=main">Inicio</a>
+                    <a href="?pagina=catalogo">Productos</a>
+                    <a href="?pagina=servicios">Servicios</a>
+                    <a href="?pagina=contacto">Contacto</a>
+                    <a href="?pagina=frecuentes">Preguntas Frecuentes</a>
             </nav>
             <i>2018 Todos los Derechos reservados.</i>
         </footer>
